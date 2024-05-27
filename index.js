2a9494c5126fc0818b23f5e479d27128244040d4
HieuNT-2306 <hieubeo2369@gmail.com>
2024-05-27 16:30:37 +0700

    Thêm metric cho prometheus

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
+import promBundle from 'express-prom-bundle';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import helmet from 'helmet';
@@ -10,6 +10,18 @@ import clientRoutes from './routes/client.js';
 
 dotenv.config();
 const app = express();
+const metricsMiddleware = promBundle({
+    includeMethod: true,
+    includePath: true,
+    includeStatusCode: true,
+    includeUp: true,
+    metrics: {
+      enabled: true,
+      prefix: 'api_'
+    }
+  });
+  
+app.use(metricsMiddleware);
 app.use(express.json());
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({policy: 'cross-origin'}));
