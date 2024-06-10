0ada26ac530b7c3a39d972c73ddd7ff9e5369c2f
HieuNT-2306 <hieubeo2369@gmail.com>
2024-06-10 12:56:09 +0700

    Thêm log cho api

--- controllers/client.js
+++ controllers/client.js
@@ -1,10 +1,23 @@
 import User from "../models/user.js";
+import logger from "../helper/logger.js";
 
 export const getUsers = async (req, res) => {
     try {
         const users = await User.find();
+        logger.info({
+            message: 'Fetched all student from the database: ',
+            path: req.originalUrl,
+            method: req.method,
+            responseCode: 200
+        });
         res.status(200).json(users);
     } catch (error) {
+        logger.error({
+            message: error.message,
+            path: req.originalUrl,
+            method: req.method,
+            responseCode: 404
+        });
         res.status(404).json({
             message: error.message,
         });
@@ -16,11 +29,29 @@ export const getUserById = async (req, res) => {
         const { id } = req.params;
         const user = await User.findById(id);
         if (!user) {
+            logger.error({
+                message: `User not found: ${id}`,
+                path: req.originalUrl,
+                method: req.method,
+                responseCode: 404
+            });
             logger.error(`User not found: ${id}`);
             return res.status(404).json({ message: 'User not found' });
         }
+        logger.info({
+            message: `Fetched user with id: ${id}`,
+            path: req.originalUrl,
+            method: req.method,
+            responseCode: 200
+        });
         res.status(200).json(user);
     } catch (error) {
+        logger.error({
+            message: error.message,
+            path: req.originalUrl,
+            method: req.method,
+            responseCode: 404
+        });
         res.status(404).json({
             message: error.message,
         });
@@ -36,20 +67,44 @@ export const postUser = async (req, res) => {
                 name, gender, school,
             });
             await newUser.save();
+            logger.info({
+                message: 'Created new student',
+                path: req.originalUrl,
+                method: req.method,
+                responseCode: 201
+            });
             res.status(201).json({ newUser });
         }
         else {
             const user = await User.findById(_id);
             if (!user) {
-                return res.status(404).json({ message: 'User not found' });
+                logger.error({
+                    message: `Student with id not found: ${_id}`,
+                    path: req.originalUrl,
+                    method: req.method,
+                    responseCode: 404
+                });
+                return res.status(404).json({ message: 'Student not found' });
             }
             if (name) user.name = name;
             if (gender) user.gender = gender;
             if (school) user.school = school;
             const updatedUser = await user.save();
+            logger.info({
+                message: `Updated student: ${_id}`,
+                path: req.originalUrl,
+                method: req.method,
+                responseCode: 200
+            });
             return res.json({ updatedUser });
         }
     } catch (error) {
+        logger.error({
+            message: error.message,
+            path: req.originalUrl,
+            method: req.method,
+            responseCode: 404
+        });
         res.status(404).json({
             message: error.message,
         });
@@ -63,12 +118,30 @@ export const deleteUser = async (req, res) => {
         const { id } = req.params;
         const user = await User.findById(id);
         if (!user) {
+            logger.error({
+                message: `Student not found: ${id}`,
+                path: req.originalUrl,
+                method: req.method,
+                responseCode: 404
+            });
             return res.status(404).json({ message: 'User not found' });
         }
         await user.deleteOne({ _id: id });
-        res.json({ message: 'User deleted successfully', user });
+        logger.info({
+            message: `Deleted user: ${id}`,
+            path: req.originalUrl,
+            method: req.method,
+            responseCode: 200
+        });
+        res.json({ message: 'Student deleted successfully', user });
     }
     catch (error) {
+        logger.error({
+            message: error.message,
+            path: req.originalUrl,
+            method: req.method,
+            responseCode: 404
+        });
         res.status(500).json({ message: error.message });
     }
 }
